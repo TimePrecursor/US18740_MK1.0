8719dce0ecf044975900717da775ed567f96d781
TimePrecursor <21006@fraser.school.nz>
2024-06-05 14:33:34 +1200

    Made the first window file

---
+++ Calculatorwindow1.py
@@ -0,0 +1,38 @@
+# Import all needed modules
+import tkinter.ttk as ttk
+from tkinter import *
+
+
+# set up UI
+window = Tk()
+window.config(bg='lightgrey')
+window.title("Calculator")
+window.geometry('520x300')
+window.resizable(0,0)
+
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
