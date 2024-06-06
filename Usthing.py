b30aed8357791c722d840820ccc7698e34b44652
TimePrecursor <21006@fraser.school.nz>
2024-06-07 10:35:23 +1200

    Major Progress

--- Usthing.py
+++ Usthing.py
@@ -1,27 +1,103 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
-<html><head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="qrichtext" content="1" /><style type="text/css">
-p, li { white-space: pre-wrap; }
-</style></head><body style=" font-family:'Consolas'; font-size:10pt; font-weight:400; font-style:normal;">
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)]</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Type &quot;copyright&quot;, &quot;credits&quot; or &quot;license&quot; for more information.</p>
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">IPython 8.12.3 -- An enhanced Interactive Python.</p>
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">In [</span><span style=" font-weight:600; color:#00ff00;">1</span><span style=" color:#00ff00;">]:</span> !git add &quot;file.py&quot;</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">   ...:</span> !git commit -m &quot;My commit&quot;</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">   ...:</span> !git push origin master</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">fatal: not a git repository (or any of the parent directories): .git</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">fatal: not a git repository (or any of the parent directories): .git</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">fatal: not a git repository (or any of the parent directories): .git</p>
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">In [</span><span style=" font-weight:600; color:#00ff00;">2</span><span style=" color:#00ff00;">]:</span> !git init</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">Initialized empty Git repository in //fs1/Studenthome$/Students/21006/Desktop/us18740/us18740/.git/</p>
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">In [</span><span style=" font-weight:600; color:#00ff00;">3</span><span style=" color:#00ff00;">]:</span> !git remote add origin https://github.com/TimePrecursor/US18740_MK1.0.git</p>
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">In [</span><span style=" font-weight:600; color:#00ff00;">4</span><span style=" color:#00ff00;">]:</span> !git add temp.py</p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;">fatal: pathspec 'temp.py' did not match any files</p>
-<p style="-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><br /></p>
-<p style=" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;"><span style=" color:#00ff00;">In [</span><span style=" font-weight:600; color:#00ff00;">5</span><span style=" color:#00ff00;">]:</span> </p></body></html>
+# Import all needed modules
+import tkinter.ttk as ttk
+from tkinter import *
+
+
+# set up UI
+window = Tk()
+window.config(bg='lightgrey')
+window.title("Calculator")
+window.geometry('500x500')
+window.resizable(0,0)
+
+
+
+# Setup / constants / small functions
+placeholder_pin = IntVar()
+
+placeholder_user = StringVar()
+
+
+
+def sign_in():
+    pass
+
+
+def read_file(): 
+    global content, file_path
+    file_path = 'secrets.txt'
+    try:
+        with open(file_path, 'r') as file:
+            content = file.read()
+    except FileNotFoundError:
+        f = open("secrets.txt", "x")
+        f.close()
+
+
+def writeto_file():
+    read_file()
+    user_info = (f"Pin: {placeholder_pin.get()} | User: {placeholder_user.get()}")
+    content_to_uplaod = (f"{content}\n{user_info}")
+    try:
+        with open(file_path, 'w') as file:
+            file.write(content_to_uplaod)
+    except FileNotFoundError:
+        print("File can't be made.")
+
+
+# Password and Username functions
+
+
+def check_username():
+    writeto_file()
+    try:
+        with open(file_path, 'r') as file:
+            x = "lol"
+            linesnum = file.readlines()
+            for count, element in enumerate(linesnum):
+                if x in element:
+                    print(f"FOUND YA {x}, YOU WERE IN LINE {count}")
+    except:
+        print("lol")
+# Score
+
+
+
+
+
+# Percentage
+
+
+
+
+
+
+# Threshold
+
+
+
+
+
+
+# Average Percentage
+
+
+
+
+
+# FINAL set up UI
+
+
+passlabel = Label(window, text = 'Pin:', font = ('calibre',10,'bold')).pack(pady=10)
+passwentry = Entry(window, textvariable = placeholder_pin, show = '*').pack()
+
+userlabel = Label(window, text = 'Username:', font = ('calibre',10,'bold')).pack(pady=10)
+userwentry = Entry(window, textvariable = placeholder_user).pack()
+
+signin_btn = Button(window, text = 'Sign in', command = check_username).pack(pady=20)
+
+
+
+
+# Mainloop
+window.mainloop()
