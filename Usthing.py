331eefb212b9ca3bb28a4bd859b34b02217246a8
TimePrecursor <131825199+TimePrecursor@users.noreply.github.com>
2024-06-18 12:10:32 +1200

    Initial base-code

--- Usthing.py
+++ Usthing.py
@@ -13,19 +13,37 @@ window.resizable(0,0)
 
 
 # Setup / constants / small functions
-placeholder_pin = IntVar()
+placeholder_pin = StringVar()
 
 placeholder_user = StringVar()
 
+placeholder_user_lower = (placeholder_user.get()).lower()
+
+global file_path
+file_path = 'secrets.txt'
 
 
-def sign_in():
-    pass
+def sign_in_Step1():
+    with open(file_path, 'r') as file:
+        x_pin = str(placeholder_pin.get())
+        x_user = str(placeholder_user.get())
+        linesnum = file.readlines()
+        print(linesnum)
+        indexlol = 0
+        for count, element in enumerate(linesnum):
+            print(count,element)
+            if x_user in element and (x_pin in element):
+                print(f"{x_pin} is the right password for {placeholder_user.get()}")
+                login_user2(x_user)
+        
+            # if x in element:
+            #     print(f"{x} is the right password for {placeholder_user.get()}")
+            # else:
+            #     messagebox.showerror("Error", "Invalid pin for this user.") 
 
 
 def read_file(): 
-    global content, file_path
-    file_path = 'secrets.txt'
+    global content
     try:
         with open(file_path, 'r') as file:
             content = file.read()
@@ -36,7 +54,7 @@ def read_file():
 
 def writeto_file():
     read_file()
-    user_info = (f"Pin: {placeholder_pin.get()} | User: {placeholder_user.get()}")
+    user_info = (f" User: {placeholder_user_lower} | Pin: {placeholder_pin.get()}")
     content_to_uplaod = (f"{content}\n{user_info}")
     try:
         with open(file_path, 'w') as file:
@@ -47,18 +65,52 @@ def writeto_file():
 
 # Password and Username functions
 
-
 def check_username():
-    writeto_file()
+    # writeto_file()
     try:
         with open(file_path, 'r') as file:
-            x = "lol"
+            x = placeholder_user.get()
             linesnum = file.readlines()
             for count, element in enumerate(linesnum):
                 if x in element:
                     print(f"FOUND YA {x}, YOU WERE IN LINE {count}")
+                    check_pin()
+                else:
+                    messagebox.showerror("Error", "User not found!") 
     except:
-        print("lol")
+        print("ez")
+
+
+def check_pin():
+    x = placeholder_pin.get()
+    print(x)
+    try:
+        y = int(x)+2
+        # y2 = str(x).split()
+        # print(len(y2))
+        # if len(y2) != 4:
+        #     raise Exception(TypeError)
+    except:
+        messagebox.showerror("Error", "Invalid pin type.") 
+    else:
+        sign_in_Step1()
+        
+        # boolx = len(str(x)) != 4
+        # if boolx == True:
+        #     messagebox.showerror("error", "Please enter a valid pin (four digits)") 
+        # elif boolx == False:
+        #     check_username()
+
+
+# the actual login function that splits the user to two different databases
+def login_user2(username):
+    if username == "Lou" or "lou":
+        pass
+        
+    elif username == "Lewis" or "lewis":    
+        pass
+        
+
 # Score
 
 
@@ -86,15 +138,14 @@ def check_username():
 
 
 # FINAL set up UI
-
-
-passlabel = Label(window, text = 'Pin:', font = ('calibre',10,'bold')).pack(pady=10)
-passwentry = Entry(window, textvariable = placeholder_pin, show = '*').pack()
-
 userlabel = Label(window, text = 'Username:', font = ('calibre',10,'bold')).pack(pady=10)
 userwentry = Entry(window, textvariable = placeholder_user).pack()
 
-signin_btn = Button(window, text = 'Sign in', command = check_username).pack(pady=20)
+passlabel = Label(window, text = 'Pin:', font = ('calibre',10,'bold')).pack(pady=10)
+passwentry = Entry(window, textvariable = placeholder_pin).pack()
+
+
+signin_btn = Button(window, text = 'Sign in', command = check_pin).pack(pady=20)
 
 
 
